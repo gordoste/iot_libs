c8726270cf30ad31e2db1c1c12b2eb9949a078f7
gordoste <gordoste@iinet.net.au>
2019-08-26 22:22:35 +1000

    Add strtoke()

---
+++ src/StringUtils.cpp
@@ -0,0 +1,20 @@
+#include "StringUtils.h"
+
+// Behaves like strtok() except that it returns empty tokens also
+char* strtoke(char *str, const char *delim)
+{
+  static char *start = NULL; /* stores string str for consecutive calls */
+  char *token = NULL; /* found token */
+  /* assign new start in case */
+  if (str) start = str;
+  /* check whether text to parse left */
+  if (!start) return NULL;
+  /* remember current start as found token */
+  token = start;
+  /* find next occurrence of delim */
+  start = strpbrk(start, delim);
+  /* replace delim with terminator and move start to follower */
+  if (start) *start++ = '\0';
+  /* done */
+  return token;
+}
---
+++ src/StringUtils.h
@@ -0,0 +1,9 @@
+#ifndef _STRINGUTILS_H
+#define _STRINGUTILS_H
+
+#include <stdio.h>
+#include <string.h>
+
+char* strtoke(char *str, const char *delim);
+
+#endif // #ifndef _STRINGUTILS_H
