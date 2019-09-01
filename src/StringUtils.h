8c85ed8ca7803dcdea525b6eb8bbc141eedf2242
gordoste <gordoste@iinet.net.au>
2019-09-01 23:12:33 +1000

    Add multiChoice()

--- src/StringUtils.h
+++ src/StringUtils.h
@@ -8,8 +8,14 @@
 
 namespace StringUtils {
     char* strtoke(char *str, const char *delim);
-    static uint8_t c1;  // Last character buffer
     uint8_t utf8ascii(uint8_t ascii);
+    char serialRead();
+    // Return number (1-9) of selected choice, or 0 for lastChoice. -1 on error (or user cancelled).
+    int8_t multiChoice(const char *choices[], uint8_t numChoices, Stream *out,
+        int8_t selectedChoice = -1, const char *lastChoice = NULL, char (*inputRead)() = &serialRead);
+    // Return selected choice. -1 on error (or user cancelled).
+    char multiChoice(const char *choiceStrings[], const char *choices, Stream *out,
+        char selectedChoice = '\0', char (*inputRead)() = &serialRead);        
 }
 
 #endif // #ifndef _STRINGUTILS_H
