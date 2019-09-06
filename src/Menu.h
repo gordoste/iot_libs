1b922cc587dc1305e06a5a774f29cf9576621263
gordoste <gordoste@iinet.net.au>
2019-09-07 00:52:18 +1000

    Nasty function pointers for reading input

--- src/Menu.h
+++ src/Menu.h
@@ -2,25 +2,26 @@
 #define _MENU_H
 
 #include <Arduino.h>
+#include "ExtendedSerial.h"
 #include "StringUtils.h"
 
 class Menu {
   public:
     // Return number (1-9) of selected choice, or 0 for lastChoice. -1 on error (or user cancelled).
-    virtual int8_t multiChoice(const char *choices[], uint8_t numChoices, int8_t selectedChoice, const char *lastChoice);
+    virtual int8_t multiChoice(const char *choices[], uint8_t numChoices, int8_t selectedChoice, const char *lastChoice) = 0;
     // Return selected choice. -1 on error (or user cancelled).
-    virtual char multiChoice(const char *choiceStrings[], const char *choices, char selectedChoice);
+    virtual char multiChoice(const char *choiceStrings[], const char *choices, char selectedChoice) = 0;
 };
 
-class StreamMenu : Menu {
+class StreamMenu : public Menu {
   public:
-    void begin(Stream *stdIn, Print *stdOut);
+    void begin(char (ExtendedSerial::*inputRead)(), Print *stdOut);
     // Return number (1-9) of selected choice, or 0 for lastChoice. -1 on error (or user cancelled).
     int8_t multiChoice(const char *choices[], uint8_t numChoices, int8_t selectedChoice = -1, const char *lastChoice = NULL);
     // Return selected choice. -1 on error (or user cancelled).
     char multiChoice(const char *choiceStrings[], const char *choices, char selectedChoice = '\0');
   private:
-    Stream *m_stdin;
+    char (ExtendedSerial::*m_inputRead)();
     Print *m_stdout;
 };
 
@@ -28,7 +29,7 @@ class StreamMenu : Menu {
 
 #include "TFT_eSPI.h"
 
-class TFTMenu : Menu {
+class TFTMenu : public Menu {
   public:
     void begin(TFT_eSPI *tft);
     // Return number (1-9) of selected choice, or 0 for lastChoice. -1 on error (or user cancelled).
