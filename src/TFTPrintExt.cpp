e31d2f8d99568dc80b83f772b4b8c5ecb22f420a
gordoste <gordoste@iinet.net.au>
2019-09-07 23:31:14 +1000

    Handle CR and reaching end of screen

--- src/TFTPrintExt.cpp
+++ src/TFTPrintExt.cpp
@@ -8,7 +8,15 @@ void TFTPrintExt::begin(TFT_eSPI *tft)
 size_t TFTPrintExt::write(uint8_t ch)
 {
     if (ch == 0) return 0;
+    if (ch == '\r') {
+        m_tft->setCursor(0, m_tft->getCursorY());
+        return 1;
+    }
     m_tft->write(ch);
+    if (m_tft->getCursorY() >= m_tft->height()) {
+        m_tft->fillScreen(TFT_BLACK);
+        m_tft->setCursor(0,0);
+    }   
     return 1; // Processed 1 character
 }
 
