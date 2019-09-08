503be9588913f7a8fa751e8a8c00b04f6fe20ca6
gordoste <gordoste@iinet.net.au>
2019-09-08 22:30:14 +1000

    wip - Implement TFTMenu.multiChoice

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -58,11 +58,76 @@ void TFTMenu::begin(TFT_eSPI *tft) { m_tft = tft; }
 // Return number (1-9) of selected choice, or 0 for lastChoice. -1 on error (or user cancelled).
 int8_t TFTMenu::multiChoice(const char *choices[], uint8_t numChoices,
   int8_t selectedChoice /*= -1*/, const char *lastChoice /*= NULL*/) {
-
-
 }
 
 // Return selected choice. -1 on error (or user cancelled).
 char TFTMenu::multiChoice(const char *choiceStrings[], const char *choices, char selectedChoice /*= '\0'*/) {
+  uint8_t numChoices = strlen(choices);
+  uint8_t size = sqrt(numChoices)+1;
+  Window win = { 0, 0, m_tft->width(), m_tft->height()-100 };
+  int8_t r = selectGrid(win, size, size, numChoices, menuColourCombos, NUM_COMBOS, TFT_WHITE, 1, choiceStrings);
+  if (r == -1) return -1;
+  return choices[r];
+}
 
+
+void TFTMenu::drawBorderRect(Window win, uint32_t borderColor, uint32_t fillColor, uint8_t borderWidth /*= 1*/) {
+  for (int i = 0; i < borderWidth; i++) {
+    m_tft->drawRect(win.x+i, win.y+i, win.width-(2*i), win.height-(2*i), borderColor);
+  }
+  m_tft->fillRect(win.x+borderWidth, win.y+borderWidth, win.width-2*borderWidth, win.height-2*borderWidth, fillColor);
+}
+
+int8_t TFTMenu::selectGrid(Window win, int32_t xDivs, int32_t yDivs, uint8_t numChoices, TCellLabel_Getter labelGetter,
+  const uint32_t colourCombos[], uint8_t numColourCombos, uint32_t borderColor, uint8_t borderWidth) {
+    uint32_t divX = win.width/xDivs; // size of each cell
+    uint32_t divY = win.height/yDivs;
+    uint8_t currentDiv[2] = { 0, 0 }; // which cell are we drawing
+    uint8_t currentColourCombo = 0;
+    Window cell;
+    while (currentDiv[0] < xDivs && currentDiv[1] < yDivs && (currentDiv[0] + currentDiv[1]*xDivs) < numChoices) {
+      cell.x = win.x + currentDiv[0] * divX;
+      cell.y = win.y + currentDiv[1] * divY;
+      cell.width = currentDiv[0] == xDivs-1 ? win.width - (divX * currentDiv[0]) : divX;   // if last cell in row, use remaining pixels
+      cell.height = currentDiv[1] == yDivs-1 ? win.height - (divY * currentDiv[1]) : divY; // if last row in grid, use remaining pixels
+      drawBorderRect(cell, TFT_WHITE, colourCombos[2*currentColourCombo]);
+      const char *label;
+      if (labelGetter != NULL) {
+        label = labelGetter(currentDiv[0] + currentDiv[1]*xDivs);
+        if (label != NULL) {
+          m_tft->setCursor(win.x + currentDiv[0]*divX + 2, win.y + currentDiv[1]*divY + 2);
+          m_tft->setTextColor(colourCombos[2*currentColourCombo + 1]);
+          m_tft->printf(label);
+        }
+      }
+      currentColourCombo++; currentColourCombo %= numColourCombos;
+      if (++currentDiv[0] == xDivs) { currentDiv[1]++; currentDiv[0] = 0; } // Increment cell, move to next row if needed
+    }
+    uint16_t touchX, touchY;
+    int8_t cellSelected = -1;
+    while (cellSelected < 0 || cellSelected >= numChoices) {
+      cellSelected = -1;
+      while (!m_tft->getTouch(&touchX, &touchY)) {} 
+      currentDiv[0] = 0; currentDiv[1] = 0;
+      while (touchX < win.x + currentDiv[0]*divX || touchX >= win.x + ++currentDiv[0]*divX) {} // Find the col AFTER the one touched
+      while (touchY < win.y + currentDiv[1]*divY || touchY >= win.y + ++currentDiv[1]*divY) {} // Find the row AFTER the one touched
+      currentDiv[0]--; currentDiv[1]--; // Find the cell touched
+      if (currentDiv[0] == xDivs || currentDiv[1] == yDivs) return -1;
+      cellSelected = currentDiv[0] + currentDiv[1]*xDivs;
+    }
+    return cellSelected;
+}
+
+int8_t TFTMenu::selectGrid(Window win, int32_t xDivs, int32_t yDivs, uint8_t numChoices,
+  const uint32_t colourCombos[], uint8_t numColourCombos, uint32_t borderColor, uint8_t borderWidth,
+  const char *cellLabels[]) {
+    if (cellLabels != NULL) {
+      m_cellLabels = cellLabels;
+      return selectGrid(win,xDivs,yDivs,numChoices,[this](uint8_t i){return m_cellLabels[i];},
+        colourCombos,numColourCombos,borderColor,borderWidth);//this->getCellLabel);
+    }
+    else {
+      return selectGrid(win,xDivs,yDivs,numChoices,[this](uint8_t i){return (const char *)NULL;},
+        colourCombos,numColourCombos,borderColor,borderWidth);
+    }
 }
--- src/Menu.h
+++ src/Menu.h
@@ -29,6 +29,26 @@ class StreamMenu : public Menu {
 
 #include "TFT_eSPI.h"
 
+typedef std::function<const char *(uint8_t cell_nr)> TCellLabel_Getter;
+
+struct Window {
+  uint32_t x;
+  uint32_t y;
+  int16_t width;
+  int16_t height;
+};
+
+#define NUM_COMBOS 6
+const uint32_t menuColourCombos[2*NUM_COMBOS] = {
+  // Fill colout    Text colour
+  TFT_YELLOW,       TFT_BLACK,
+  TFT_ORANGE,       TFT_BLACK,
+  TFT_BLUE,         TFT_WHITE,
+  TFT_CYAN,         TFT_BLACK,
+  TFT_MAGENTA,      TFT_BLACK,
+  TFT_DARKGREEN,    TFT_WHITE,
+};
+
 class TFTMenu : public Menu {
   public:
     void begin(TFT_eSPI *tft);
@@ -36,8 +56,16 @@ class TFTMenu : public Menu {
     int8_t multiChoice(const char *choices[], uint8_t numChoices, int8_t selectedChoice = -1, const char *lastChoice = NULL);
     // Return selected choice. -1 on error (or user cancelled).
     char multiChoice(const char *choiceStrings[], const char *choices, char selectedChoice = '\0');
+    void drawBorderRect(Window win, uint32_t borderColor, uint32_t fillColor, uint8_t borderWidth = 1);
+    const char *getCellLabel(uint8_t cell_nr) { return m_cellLabels[cell_nr]; }
+    int8_t selectGrid(Window win, int32_t xDivs, int32_t yDivs, uint8_t numChoices, TCellLabel_Getter labelGetter,
+      const uint32_t colourCombos[], uint8_t numColourCombos, uint32_t borderColor = TFT_WHITE, uint8_t borderWidth = 1);
+    int8_t selectGrid(Window win, int32_t xDivs, int32_t yDivs, uint8_t numChoices,
+      const uint32_t colourCombos[], uint8_t numColourCombos, uint32_t borderColor = TFT_WHITE, uint8_t borderWidth = 1,
+      const char *cellLabels[] = NULL);
   protected:
     TFT_eSPI *m_tft;
+    const char **m_cellLabels;
 };
 
 #endif //#ifdef TFT_ENABLED
