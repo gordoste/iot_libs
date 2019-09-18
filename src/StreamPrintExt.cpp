a6997ea074d2afd1909583276fc34c9ccbd10f9e
gordoste <gordoste@iinet.net.au>
2019-09-18 22:11:26 +1000

    Add flush on newline option

--- src/StreamPrintExt.cpp
+++ src/StreamPrintExt.cpp
@@ -2,7 +2,14 @@
 
 void StreamPrintExt::begin(Stream *str) { m_str = str; }
 
-size_t StreamPrintExt::write(uint8_t ch) { return m_str->write(ch); }
+size_t StreamPrintExt::write(uint8_t ch) {
+    if (m_flushOnNL && ch == '\n') m_str->flush();
+    return m_str->write(ch);
+}
+
+void StreamPrintExt::setFlushOnNewline(bool flushOnNL) {
+    this->m_flushOnNL = flushOnNL;
+}
 
 #if !defined(NO_GLOBAL_INSTANCES) && !defined(NO_GLOBAL_STREAMPRINTEXT)
 StreamPrintExt StreamExt;
--- src/StreamPrintExt.h
+++ src/StreamPrintExt.h
@@ -9,8 +9,10 @@ public:
   StreamPrintExt(){};
   void begin(Stream *str);
   virtual size_t write(uint8_t ch);
+  void setFlushOnNewline(bool flushOnNL);
 private:
   Stream *m_str;
+  bool m_flushOnNL = false;
 };
 
 #if !defined(NO_GLOBAL_INSTANCES) && !defined(NO_GLOBAL_STREAMPRINTEXT)
