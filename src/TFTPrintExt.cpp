97e7ac4092ef38b601f7ef3e14ee781f1bd3e5ff
gordoste <gordoste@iinet.net.au>
2019-09-21 15:32:49 +1000

    Add fill color and text color properties and onWrite handler.

--- src/TFTPrintExt.cpp
+++ src/TFTPrintExt.cpp
@@ -11,11 +11,14 @@ void TFTPrintExt::begin(TFT_eSPI *tft, Window *win)
 }
 
 void TFTPrintExt::clear() {
-    TFTUtils::fillWindow(m_tft, *m_win, TFT_BLACK);
+    TFTUtils::fillWindow(m_tft, *m_win, m_fillColor);
     if (m_win == NULL) m_tft->setCursor(0, 0);
     else m_tft->setCursor(m_win->x, m_win->y);
     m_cursor = { m_tft->getCursorX(), m_tft->getCursorY() };
 }
+
+void TFTPrintExt::setFillColor(uint32_t fillColor) { m_fillColor = fillColor; }
+void TFTPrintExt::setTextColor(uint32_t textColor) { m_textColor = textColor; }
 
 size_t TFTPrintExt::write(uint8_t ch)
 {
@@ -24,9 +27,9 @@ size_t TFTPrintExt::write(uint8_t ch)
         m_tft->setCursor(0, m_cursor.y);
     }
     else {
+        if (onWriteFn != NULL) { onWriteFn(m_tft); }
         m_tft->setCursor(m_cursor.x, m_cursor.y);
-        m_tft->setTextFont(1);
-        m_tft->setTextColor(TFT_WHITE);
+        m_tft->setTextColor(m_textColor);
         m_tft->write(ch);
         if (m_tft->getCursorY() >= m_tft->height()) clear();   
         m_cursor = { m_tft->getCursorX(), m_tft->getCursorY() };
--- src/TFTPrintExt.h
+++ src/TFTPrintExt.h
@@ -13,11 +13,17 @@ class TFTPrintExt : public PrintExt
     TFTPrintExt(){};
     void begin(TFT_eSPI *tft, Window *win = NULL);
     void clear();
+    void setFillColor(uint32_t fillColor);
+    void setTextColor(uint32_t textColor);
+    void onWrite(TFTHandler fn) { onWriteFn = fn; };
     virtual size_t write(uint8_t ch);
   private:
     TFT_eSPI *m_tft;
     Window *m_win;
     Point m_cursor;
+    uint32_t m_fillColor = TFT_BLACK;
+    uint32_t m_textColor = TFT_WHITE;
+    TFTHandler onWriteFn = NULL;
 };
 
 #if !defined(NO_GLOBAL_INSTANCES) && !defined(NO_GLOBAL_EXTENDEDSERIAL)
