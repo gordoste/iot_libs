5a85503d4786b5ecb8ea8a2c29789245ab7b7333
gordoste <gordoste@iinet.net.au>
2020-04-30 22:42:36 +1000

    Handle relative co-ords in touch handler. Ignore touches for a short time after handling one.

--- src/TFTUtils.cpp
+++ src/TFTUtils.cpp
@@ -1,59 +1,63 @@
 #include "TFTUtils.h"
 
 Window *TouchControl::getWindow() { return m_win; }
-void TouchControl::handleTouch(uint16_t x, uint16_t y) { m_touchHandler(x,y); }
-
-void TFTUtils::fillWindow(TFT_eSPI *tft, Window win, uint32_t fillColor) {
-  tft->fillRect(win.x, win.y, win.width, win.height, fillColor);
+void TouchControl::handleTouch(uint16_t x, uint16_t y, bool absolute) {
+    if (m_lastTouchTime + TFT_TOUCH_TIME > millis()) return;
+    m_touchHandler(x, y, absolute);
+    m_lastTouchTime = millis();
 }
 
-void TFTUtils::ignoreTouch(uint16_t x, uint16_t y) { return; }
+void TFTUtils::fillWindow(TFT_eSPI *tft, Window win, uint32_t fillColor) {
+    tft->fillRect(win.x, win.y, win.width, win.height, fillColor);
+}
+
+void TFTUtils::ignoreTouch(uint16_t x, uint16_t y, bool absolute) { return; }
 
 void TFTUtils::drawBorderRect(TFT_eSPI *tft, Window win, uint32_t fillColor, LineProperties borderProps) {
-  for (int i = 0; i < borderProps.width; i++) {
-    tft->drawRect(win.x+i, win.y+i, win.width-(2*i), win.height-(2*i), borderProps.color);
-  }
-  tft->fillRect(win.x + borderProps.width, win.y + borderProps.width,
-    win.width - (2*borderProps.width), win.height - (2*borderProps.width), fillColor);
+    for (int i = 0; i < borderProps.width; i++) {
+        tft->drawRect(win.x + i, win.y + i, win.width - (2 * i), win.height - (2 * i), borderProps.color);
+    }
+    tft->fillRect(win.x + borderProps.width, win.y + borderProps.width,
+                  win.width - (2 * borderProps.width), win.height - (2 * borderProps.width), fillColor);
 }
 
 void TFTUtils::touchCalibrate(TFT_eSPI *tft, const char *calFileName, bool forceCalibrate) {
-  uint16_t calData[5];
-  uint8_t calDataOK = 0;
+    uint16_t calData[5];
+    uint8_t calDataOK = 0;
 
-  // check if calibration file exists
-  if (SPIFFS.exists(calFileName)) {
-    File f = SPIFFS.open(calFileName, "r");
-    if (f) {
-      size_t rd = f.readBytes((char *)calData, 14);
-      if (rd == 14) {
-        calDataOK = 1;
-      }
-      f.close();
+    // check if calibration file exists
+    if (SPIFFS.exists(calFileName)) {
+        File f = SPIFFS.open(calFileName, "r");
+        if (f) {
+            size_t rd = f.readBytes((char *)calData, 14);
+            if (rd == 14) {
+                calDataOK = 1;
+            }
+            f.close();
+        }
     }
-  }
-  if (calDataOK && !forceCalibrate) {
-    // calibration data valid
-    tft->setTouch(calData);
-  } else {
-    tft->fillScreen(TFT_BLACK);
-    tft->setTextColor(TFT_WHITE, TFT_BLACK);
-    tft->setTextSize(1);
-    tft->setCursor(20, 0, 2);
-    tft->println("calibration run");
-    // data not valid. recalibrate
-    tft->calibrateTouch(calData, TFT_MAGENTA, TFT_BLACK, 15);
-    // store data
-    File f = SPIFFS.open(calFileName, "w");
-    if (f) {
-      f.write((const unsigned char *)calData, 14);
-      f.close();
+    if (calDataOK && !forceCalibrate) {
+        // calibration data valid
+        tft->setTouch(calData);
+    } else {
+        tft->fillScreen(TFT_BLACK);
+        tft->setTextColor(TFT_WHITE, TFT_BLACK);
+        tft->setTextSize(1);
+        tft->setCursor(20, 0, 2);
+        tft->println("calibration run");
+        // data not valid. recalibrate
+        tft->calibrateTouch(calData, TFT_MAGENTA, TFT_BLACK, 15);
+        // store data
+        File f = SPIFFS.open(calFileName, "w");
+        if (f) {
+            f.write((const unsigned char *)calData, 14);
+            f.close();
+        }
     }
-  }
 }
 
 bool TFTUtils::contains(Window &w, uint16_t x, uint16_t y) {
-  if (x < w.x || x > w.x + w.width) return false;
-  if (y < w.y || y > w.y + w.height) return false;
-  return true;
+    if (x < w.x || x > w.x + w.width) return false;
+    if (y < w.y || y > w.y + w.height) return false;
+    return true;
 }
--- src/TFTUtils.h
+++ src/TFTUtils.h
@@ -7,6 +7,8 @@
 
 #ifdef TFT_ENABLED
 
+#define TFT_TOUCH_TIME 100 // ms to ignore touches after processing one
+
 struct Point {
   uint32_t x;
   uint32_t y;
@@ -28,10 +30,10 @@ const LineProperties DefaultBorder = { TFT_WHITE, 1 };
 
 typedef std::function<void(TFT_eSPI *tft)> TFTHandler;
 
-typedef std::function<void(uint16_t x, uint16_t y)> TouchHandler;
+typedef std::function<void(uint16_t x, uint16_t y, bool absolute)> TouchHandler;
 
 namespace TFTUtils {
-  void ignoreTouch(uint16_t x, uint16_t y);
+  void ignoreTouch(uint16_t x, uint16_t y, bool absolute);
   bool contains(Window &w, uint16_t x, uint16_t y);
   void fillWindow(TFT_eSPI *tft, Window win, uint32_t fillColor);
   void drawBorderRect(TFT_eSPI *tft, Window win, uint32_t fillColor, LineProperties borderProps = DefaultBorder);
@@ -43,9 +45,10 @@ class TouchControl {
   protected:
     Window *m_win;
     TouchHandler m_touchHandler = TFTUtils::ignoreTouch;
+    uint32_t m_lastTouchTime = 0;
   public:
     Window *getWindow();
-    void handleTouch(uint16_t x, uint16_t y); // must be relative to control position
+    void handleTouch(uint16_t x, uint16_t y, bool absolute);
 };
 
 #endif // #ifdef TFT_ENABLED
