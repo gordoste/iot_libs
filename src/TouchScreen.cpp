a7baea6dd7cceed93021f89e449cedf7e2eeeecd
gordoste <gordoste@iinet.net.au>
2020-05-07 22:56:39 +1000

    Add TouchScreen class to manage controls and do event handling

---
+++ src/TouchScreen.cpp
@@ -0,0 +1,29 @@
+#include "TouchScreen.h"
+
+void TouchScreen::init(BasicLog *log, TFT_eSPI *tft) {
+    m_log = log;
+    m_tft = tft;
+}
+
+void TouchScreen::addControl(Control *ctl, Window *win) {
+    ctl->init(m_log, m_tft, win);
+    m_listOfControls.push_back(ctl);
+}
+
+void TouchScreen::addTouchControl(TouchControl *ctl, Window *win) {
+    m_listOfTouchControls.push_back(ctl);
+    addControl(ctl, win);
+}
+
+void TouchScreen::checkForTouch() {
+    if (m_tft->getTouch(&m_touchX, &m_touchY)) {
+        std::list<TouchControl *>::iterator it = m_listOfTouchControls.begin();
+        while (it != m_listOfTouchControls.end()) {
+            Window *ctlWin = (*it)->getWindow();
+            if (TFTUtils::contains((*ctlWin), m_touchX, m_touchY)) {
+                (*it)->handleTouch(m_touchX, m_touchY, true);
+            }
+            it++;
+        }
+    }
+}
---
+++ src/TouchScreen.h
@@ -0,0 +1,24 @@
+#ifndef _TOUCHSCREEN_H
+#define _TOUCHSCREEN_H
+
+#include <list>
+
+#include "Control.h"
+#include "TouchControl.h"
+#include "TFTUtils.h"
+
+class TouchScreen {
+    public:
+        void init(BasicLog *log, TFT_eSPI *tft);
+        void addControl(Control *ctl, Window *win);
+        void addTouchControl(TouchControl *ctl, Window *win);
+        void checkForTouch();
+    private:
+        BasicLog *m_log;
+        TFT_eSPI *m_tft;
+        std::list<Control *> m_listOfControls;
+        std::list<TouchControl *> m_listOfTouchControls;
+        uint16_t m_touchX, m_touchY;    // Store touch co-ordinates
+};
+
+#endif // #ifndef _TOUCHSCREEN_H
