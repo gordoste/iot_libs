eb5b5e2fcd1586ddd6e764e095a8fe75eaa82e18
gordoste <gordoste@iinet.net.au>
2020-05-22 22:31:41 +1000

    Implement text padding

--- src/ui/PrintExtBox.cpp
+++ src/ui/PrintExtBox.cpp
@@ -1,6 +1,7 @@
 #include "PrintExtBox.h"
 
 void PrintExtBox::init() {
+    m_textPadding = 1;
     if (m_win == NULL) {
         m_cursor = {0, 0};
     } else {
@@ -10,7 +11,7 @@ void PrintExtBox::init() {
 
 void PrintExtBox::paintBG() {
     Control::paintBG();
-    m_tft->setCursor(m_win->x + m_borderProps.width + 1, m_win->y + m_borderProps.width + 1);
+    m_tft->setCursor(m_win->x + m_borderProps.width + m_textPadding, m_win->y + m_borderProps.width + m_textPadding);
     m_cursor = {m_tft->getCursorX(), m_tft->getCursorY()};
 }
 
@@ -19,9 +20,7 @@ Point &PrintExtBox::getCursor() { return m_cursor; }
 size_t PrintExtBox::write(uint8_t ch) {
     if (!m_shown) return 0;
     if (ch == 0) return 0;
-    if (ch == '\r') {
-        m_tft->setCursor(m_win->x + m_borderProps.width + 1, m_cursor.y);
-    } else {
+    if (ch != '\r') {
         if (onWriteFn != NULL) {
             onWriteFn(m_tft);
         }
@@ -31,6 +30,7 @@ size_t PrintExtBox::write(uint8_t ch) {
         if (m_tft->getCursorY() >= m_tft->height()) paintBG();
         m_cursor = {m_tft->getCursorX(), m_tft->getCursorY()};
     }
+    if (ch == '\r' || ch == '\n') m_cursor.x = m_win->x + m_borderProps.width + m_textPadding;
     return 1; // Processed 1 character
 }
 
