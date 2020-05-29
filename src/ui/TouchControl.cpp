4635d3a9302c33db5dfee4c2906678097ca139ca
gordoste <gordoste@iinet.net.au>
2020-05-29 20:48:59 +1000

    Moved UI stuff to separate repo TFTUI

--- src/ui/TouchControl.cpp
+++
@@ -1,8 +0,0 @@
-#include "TouchControl.h"
-
-void TouchControl::handleTouch(uint16_t x, uint16_t y, bool absolute) {
-    if (!m_shown) return;
-    if (m_lastTouchTime + TFT_TOUCH_TIME > millis()) return;
-    m_touchHandler(x, y, absolute);
-    m_lastTouchTime = millis();
-}
